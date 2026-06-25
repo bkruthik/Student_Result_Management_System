299faa4b6c6c0be05d1bb1c9db791629e5908670
bkruthik <balusukruthik@gmail.com>
2026-06-25 23:20:29 +0530

    Update Student.py

--- Student.py
+++ Student.py
@@ -1,14 +1,12 @@
 import mysql.connector
 
 # Connect to MySQL
-
 conn = mysql.connector.connect(
     host="localhost",
     user="root",
     password=input("Enter the password:  "),
     database="student_db"
 )
-
 
 cursor = conn.cursor()
 def add_student():
